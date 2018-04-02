7252de2b5ff84557e11b11107cc7801b9d46c6e7
amily4555 <amily4555@gmail.com>
2018-04-02 15:19:24 +0800

    0.0.2 -> 0.1.5-b1

--- src/pages/list/page.tsx
+++ src/pages/list/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import router from 'umi/router';
 import styles from './page.less';
 // import {MrEcharts} from '../../../pub/mr-echarts/mr-echarts.component';
-// import {MrEcharts} from 'masterrt';
-import {MrEcharts} from '../../lib/mr-echarts/mr-echarts.component';
+import {MrEcharts} from 'masterrt';
+// import {MrEcharts} from '../../lib/mr-echarts/mr-echarts.component';
 // import {MrEcharts} from 'masterrt/mr-echarts/echarts.component';
 
 console.log(':::::::::');
